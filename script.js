e50802394c06002fa09bdfed98ecd1262688b968
Jaime Lopez <jaimelopezmislata@gmail.com>
2025-02-26 13:40:19 +0100

    Mejoras en la legibilidad

--- script.js
+++ script.js
@@ -2,40 +2,21 @@ const cajaLista = document.getElementById("listaDragonBall");
 
 fetch("https://dragonball-api.com/api/characters?limit=1000")
     .then((response) => response.json())
-    .then((data) => {
-        data.items.forEach((item) => {
-            console.log(item);
+    .then((listaPersonajes) => {
+        listaPersonajes.items.forEach((personaje) => {
+            /* console.log(personaje); */
             const {
-                id,
                 name,
-                ki,
-                maxKi,
-                race,
-                gender,
                 description,
                 image,
-                affiliation,
-                deletedAt,
-            } = item;
+            } = personaje;
             cajaLista.innerHTML +=
                 "<div class='cajaPersonaje'><h1>" +
                 name +
                 "</h1><img src=" +
                 image +
-                "><p><b>KI:</b> " +
-                ki +
-                "</p><p><b>MAX KI:</b> " +
-                maxKi +
-                "</p><p><b>RACE:</b> " +
-                race +
-                "</p><p><b>GENDER:</b> " +
-                gender +
-                "</p><p><b>DESCRIPTION:</b> " +
+                "><p><b>DESCRIPCIÓN:</b> " +
                 description +
-                "</p><p><b>AFFILIATION:</b> " +
-                affiliation +
-                "</p><p><b>DELETED AT:</b> " +
-                deletedAt +
                 "</p></div>";
         });
     });
