01f6b2b43be26a13cc8f4a30267a4e12098fde56
Jaime Lopez <jaimelopezmislata@gmail.com>
2025-02-27 12:02:50 +0100

    Añadido búsqueda de personajes

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
 const cajaLista = document.getElementById("listaDragonBall");
+const inputBusqueda = document.getElementById("inputBusqueda".value);
+const botonBusqueda = document.getElementById("botonBusqueda");
+botonBusqueda.addEventListener("click", buscarPersonajes);
 
 function mostrarPersonajes() {
     fetch("https://dragonball-api.com/api/characters?limit=1000")
-    .then((response) => response.json())
-    .then((listaPersonajes) => {
-        listaPersonajes.items.forEach((personaje) => {
-            /* console.log(personaje); */
-            const {
-                name,
-                description,
-                image,
-            } = personaje;
-            cajaLista.innerHTML +=
-                "<div class='cajaPersonaje'><h1>" +
-                name +
-                "</h1><img src=" +
-                image +
-                "><p><b>DESCRIPCIÓN:</b> " +
-                description +
-                "</p></div>";
-        });
-    
-    })
-.catch(error => console.log("ERROR AL CARGAR DATOS",error))
+        .then((response) => response.json())
+        .then((listaPersonajes) => {
+            listaPersonajes.items.forEach((personaje) => {
+                /* console.log(personaje); */
+                const { name, description, image } = personaje;
+                cajaLista.innerHTML +=
+                    "<div class='cajaPersonaje'><h1>" +
+                    name +
+                    "</h1><img src=" +
+                    image +
+                    "><p><b>DESCRIPCIÓN:</b> " +
+                    description +
+                    "</p></div>";
+            });
+        })
+        .catch((error) => console.log("ERROR AL OBTENER LA LISTA DE PERSONAJES", error));
 }
 
-mostrarPersonajes()
+function buscarPersonajes() {
+    fetch(
+        "https://dragonball-api.com/api/characters?name=" + inputBusqueda
+    )
+        .then((response) => response.json())
+        .then((personajesFilter) => {
+            if (inputBusqueda.value != "") {
+                cajaLista.innerHTML = "";
+                personajesFilter.forEach((personajes) => {
+                    const { name, description, image } = personajes;
+                    cajaLista.innerHTML +=
+                        "<div class='cajaPersonaje'><h1>" +
+                        name +
+                        "</h1><img src=" +
+                        image +
+                        "><p><b>DESCRIPCIÓN:</b> " +
+                        description +
+                        "</p></div>";
+                });
+            } else {
+                cajaLista.innerHTML = "";
+                mostrarPersonajes();
+            }
+        })
+        .catch((error) => console.log("ERROR AL OBTENER PERSONAJES FILTRADOS", error));
+}
+
+mostrarPersonajes();
